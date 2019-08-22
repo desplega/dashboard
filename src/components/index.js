b53265295b2319280b0f93e60ed4e761e7887f18
desplega <marcel@desplega.com>
2019-08-22 19:26:55 +0200

    Removed all options in side bar except Dashboard and User Profile

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 // Tables
 import OrderedTable from "./Tables/OrderedTable.vue";
-import SimpleTable from "./Tables/SimpleTable.vue";
 
-export { OrderedTable, SimpleTable };
+export { OrderedTable };
