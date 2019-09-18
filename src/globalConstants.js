92a2c91c11cf66a40dc435637cf1b62a61fd1c2e
desplega <marcel@desplega.com>
2019-09-18 20:25:02 +0200

    Global constants file

---
+++ src/globalConstants.js
@@ -0,0 +1,8 @@
+// Global constants
+const GlobalConstants = {
+  BASE_API_URL: "http://localhost:9000",
+  API_AUTH_TOKEN: "AUTH_TOKEN",
+  AUTH_USER: "AUTH_USER"
+};
+
+export default GlobalConstants;
