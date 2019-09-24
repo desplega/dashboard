c22dcb4f1681929a0cda2fc9cc9e1cc14d07ac1b
desplega <marcel@desplega.com>
2019-09-24 20:55:23 +0200

    View device colors changed

--- src/pages/ViewDevice.vue
+++ src/pages/ViewDevice.vue
@@ -2,13 +2,13 @@
   <div class="content">
     <div class="md-layout">
       <div class="md-layout-item md-medium-size-100 md-size-33">
-        <view-mesh-status data-background-color="purple"></view-mesh-status>
+        <view-mesh-status data-background-color="green"></view-mesh-status>
       </div>
       <div class="md-layout-item md-medium-size-100 md-size-33">
-        <view-temperature data-background-color="green"></view-temperature>
+        <view-temperature data-background-color="orange"></view-temperature>
       </div>
       <div class="md-layout-item md-medium-size-100 md-size-33">
-        <toggle-led data-background-color="orange"></toggle-led>
+        <toggle-led data-background-color="purple"></toggle-led>
       </div>
       <div class="md-layout-item md-size-100">
         <view-chart background-color="blue"></view-chart>
