0c41aba6f14f659daa6df257ab6d2d2e3b3caa0c
desplega <marcel@desplega.com>
2019-09-24 15:38:51 +0200

    Device status table redesigned.

--- src/components/Tables/OrderedTable.vue
+++ src/components/Tables/OrderedTable.vue
@@ -2,10 +2,21 @@
   <div>
     <md-table v-model="users" :table-header-color="tableHeaderColor">
       <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
         <md-table-cell md-label="Location">{{ item.location }}</md-table-cell>
-        <md-table-cell md-label="Status">{{ item.status }}</md-table-cell>
+        <md-table-cell md-label="Updated">{{ item.updated }}</md-table-cell>
+        <md-table-cell md-label="Status">
+          <md-icon :class="getClass(item)">contactless</md-icon>
+        </md-table-cell>
+        <md-table-cell md-label="Actions" class="text-right">
+          <md-button class="md-just-icon md-simple">
+            <md-icon>edit</md-icon>
+          </md-button>
+          <md-button class="md-just-icon md-simple">
+            <md-icon>delete</md-icon>
+          </md-button>
+        </md-table-cell>
       </md-table-row>
     </md-table>
   </div>
@@ -28,28 +39,49 @@ export default {
           id: 190915080614,
           name: "Rusc 1",
           location: "Olot bosc",
-          status: "OK"
+          updated: "24/09/2019 09:12:24",
+          status: "ok"
         },
         {
           id: 190915080733,
           name: "Rusc 2",
           location: "Olot bosc",
-          status: "OK"
+          updated: "24/09/2019 09:12:24",
+          status: "ok"
         },
         {
           id: 190915080848,
           name: "Rusc 3",
           location: "Olot esplanada",
-          status: "OK"
+          updated: "24/09/2019 09:12:24",
+          status: "fail"
         },
         {
           id: 190915080621,
           name: "Rusc nou",
           location: "Olot esplanada",
-          status: "OK"
+          updated: "24/09/2019 09:12:24",
+          status: "ok"
         }
       ]
     };
+  },
+  methods: {
+    getClass: function(item) {
+      return item.status == "ok" ? "status-green" : "status-red";
+    }
   }
 };
 </script>
+
+<style lang="css">
+.md-icon.status-green {
+  color: green !important;
+  padding-left: 25px;
+}
+
+.md-icon.status-red {
+  color: red !important;
+  padding-left: 25px;
+}
+</style>
