b99ed4ed0b08c074b54fb14e437dbf60eaf0eb0e
desplega <marcel@desplega.com>
2019-12-23 17:41:23 +0100

    Configure api-engine server depending on environment (development/production)

--- src/globalConstants.js
+++ src/globalConstants.js
@@ -1,6 +1,9 @@
 // Global constants
 const GlobalConstants = {
-  BASE_API_URL: "http://localhost:9000/",
+  BASE_API_URL:
+    process.env.NODE_ENV == "production"
+      ? "https://api.desplega.com/"
+      : "http://localhost:9000/",
   API_AUTH_TOKEN: "API_AUTH_TOKEN",
   AUTH_USER: "AUTH_USER",
   DATA_LIMIT: 30
